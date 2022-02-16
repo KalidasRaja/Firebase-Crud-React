8cc913c30089446f68b5d31ac70307e110a1979e
Kalidas Raja <k.s.kalidasraja@gmail.com>
2022-02-16 19:10:59 +0530

    Firebase Crud Data Updated

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Create from './pages/Create';
-import home from './pages/Home';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Update from './pages/Update';
-import Header from './Components/Header';
-
-// import Create from './pages/Create';
+import "./App.css";
+import Title from './Components/title';
+import AddData from './Components/Adddata';
+import Todo from './Components/view';
+import { collection, query, onSnapshot, doc, updateDoc, deleteDoc } from "firebase/firestore";
+import { db } from "./API_Data/firebase";
 
 function App() {
+  const [todos, setTodos] = React.useState([]);
+
+  React.useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let todosArray = [];
+      querySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArray);
+    });
+    return () => unsub();
+  }, []);
+
+  const handleEdit = async (todo, title) => {
+    await updateDoc(doc(db, "todos", todo.id), { title: title });
+  };
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), { completed: !todo.completed });
+  };
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
+
   return (
-    <BrowserRouter>
-      <Header />
-      <div className="text-black text-center text-4xl">
-        <Switch>
-          <Route exact path='/' component={home} />
-          <Route path='/Update' component={Update} />
-          <Route path='/create' component={Create} />
-        </Switch>
+    <div className=' container mx-auto'>
+      <div>
+        <Title />
       </div>
-
-    </BrowserRouter>
-
-
+      <div>
+        <AddData />
+      </div>
+      <div className="todo_container">
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            toggleComplete={toggleComplete}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
-export default App;
+
+export default App
